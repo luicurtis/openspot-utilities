3a13d5cf30c818984b49e47e85658e9e02ad2528
cwlui <cwlui@sfu.ca>
2021-10-26 09:34:43 -0700

    db upload and update finished
    - able to take a kml file that is exported from google mymaps and update the module and spot information to the db

--- planning/extract.py
+++ planning/extract.py
@@ -2,11 +2,18 @@ from pykml import parser
 from os import path
 from pathlib import Path
 
+import pymongo 
+
 PARENT_PATH = Path(__file__).parent
 KML_PATH = PARENT_PATH / Path("./kml_files")
 
 def relative_to_kml_files(path: str) -> Path:
     return KML_PATH / Path(path)
+
+MongoDBclient = pymongo.MongoClient("mongodb+srv://root:root@cluster0.56jzb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+db_name = MongoDBclient["Backend"]
+spots_db = db_name["spots"]
+module_db = db_name["module"]
 
 kml_file = path.join(relative_to_kml_files('kensington.kml'))
 
@@ -21,8 +28,10 @@ with open(kml_file) as f:
         modID = module.name.text
         print('modID: ' + modID)
 
+        totalSpots = 0
         # loop through all spots of the module
         for spot in module.Placemark:
+            totalSpots += 1
             spotID = spot.name.text
             print('spotID: ' + spotID)
             spotCoords = spot.Polygon.outerBoundaryIs.LinearRing.coordinates.text.split('\n')
@@ -32,18 +41,45 @@ with open(kml_file) as f:
             spotCoords.pop(0)
             spotCoords.pop(5)
 
-            parsedCoords = []
+            polygons = []
             for i,coord in enumerate(spotCoords):
                 splitCoord = coord.strip().split(',')
                 splitCoord.pop(2) # remove useless 0
-                parsedCoords.append({
-                    'lat': splitCoord[1],    # second element is lat
-                    'lng': splitCoord[0]     # first element is long
+                polygons.append({
+                    'lat': float(splitCoord[1]),    # second element is lat
+                    'lng': float(splitCoord[0])     # first element is long
                 })
                 # splittedCoord = strippedCoord.split(',')
                 # print(str(i) + ': ' + strippedCoord)
                 # print(type(splitCoord))
                 # print(splitCoord)
 
-            print(parsedCoords)
+            print(polygons)
             # print(len(parsedCoords))
+            
+            # insert spot record into db
+            spotIdentifier = {  'parkingLotName' : parkingLotName,
+                                'modID': int(modID),
+                                'spotNum': int(spotID)}
+
+            spotRecord = {  'parkingLotName' : parkingLotName,
+                            'modID': int(modID),
+                            'spotNum': int(spotID),
+                            'occupied': False,
+                            'polygons': polygons
+                            }
+            
+            spots_db.update_one(spotIdentifier, {"$set":spotRecord}, upsert=True)
+
+        # insert module record into db
+        print(totalSpots)
+        moduleIdentifier = {'parkingLotName' : parkingLotName,
+                            'modID': int(modID)}
+
+        moduleRecord = {'parkingLotName' : parkingLotName,
+                        'ledColour' : 0,
+                        'numSpotsFull': 0,
+                        'totalSpots' : totalSpots,
+                        'modID': int(modID)}
+
+        module_db.update_one(moduleIdentifier, {"$set":moduleRecord}, upsert=True)
