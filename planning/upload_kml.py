278a8bdbf98442be2c5da337171795b15f94fcf6
cwlui <cwlui@sfu.ca>
2021-10-26 10:02:11 -0700

    added arg parsing for specifying the kml file

--- planning/upload_kml.py
+++ planning/upload_kml.py
@@ -3,37 +3,38 @@ from os import path
 from pathlib import Path
 
 import pymongo 
+import argparse
 
-PARENT_PATH = Path(__file__).parent
-KML_PATH = PARENT_PATH / Path("./kml_files")
+def parse_args():
+    parser = argparse.ArgumentParser(description='Upload Parking Lot Spot information to .kml file to database')
 
-def relative_to_kml_files(path: str) -> Path:
-    return KML_PATH / Path(path)
+    parser.add_argument("-kml",
+                        dest="kml_file",
+                        required=True,
+                        help="kml map file to extract coordinates and upload to database")
+    return parser.parse_args()
+
+args = parse_args()
+kml_file = args.kml_file
 
 MongoDBclient = pymongo.MongoClient("mongodb+srv://root:root@cluster0.56jzb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 db_name = MongoDBclient["Backend"]
 spots_db = db_name["spots"]
 module_db = db_name["module"]
 
-kml_file = path.join(relative_to_kml_files('kensington.kml'))
-
 with open(kml_file) as f:
     doc = parser.parse(f).getroot()
-    # print(dir(doc))
     parkingLotName = doc.Document.name.text
-    print('Parking Lot Name: ' + parkingLotName)
 
     # loop through each module
     for module in doc.Document.Folder:
         modID = module.name.text
-        print('modID: ' + modID)
 
         totalSpots = 0
         # loop through all spots of the module
         for spot in module.Placemark:
             totalSpots += 1
             spotID = spot.name.text
-            print('spotID: ' + spotID)
             spotCoords = spot.Polygon.outerBoundaryIs.LinearRing.coordinates.text.split('\n')
 
             # clean up parsed data
@@ -49,14 +50,7 @@ with open(kml_file) as f:
                     'lat': float(splitCoord[1]),    # second element is lat
                     'lng': float(splitCoord[0])     # first element is long
                 })
-                # splittedCoord = strippedCoord.split(',')
-                # print(str(i) + ': ' + strippedCoord)
-                # print(type(splitCoord))
-                # print(splitCoord)
 
-            print(polygons)
-            # print(len(parsedCoords))
-            
             # insert spot record into db
             spotIdentifier = {  'parkingLotName' : parkingLotName,
                                 'modID': int(modID),
@@ -72,7 +66,6 @@ with open(kml_file) as f:
             spots_db.update_one(spotIdentifier, {"$set":spotRecord}, upsert=True)
 
         # insert module record into db
-        print(totalSpots)
         moduleIdentifier = {'parkingLotName' : parkingLotName,
                             'modID': int(modID)}
 
