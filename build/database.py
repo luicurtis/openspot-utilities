9ee398391fdda5df72dd15e09445009f4af367d7
cwlui <cwlui@sfu.ca>
2021-11-21 13:26:37 -0800

    updated spot coordinates to upload 4 points
    - still need to package it up

--- build/database.py
+++ build/database.py
@@ -43,7 +43,10 @@ class database:
         #upload the coordinates used by the computer vision
         for spotnum, bbox in enumerate(parsed_ref):
             coord = bbox["coordinates"]
-            newSpot = ([coord[0][1], coord[0][0]], [coord[2][1], coord[2][0]])
+            newSpot = ([coord[0][1], coord[0][0]],  # top left
+                        [coord[1][1], coord[1][0]], # top right
+                        [coord[2][1], coord[2][0]], # bottom right
+                        [coord[3][1], coord[3][0]]) # bottom left
             refSpots.append(newSpot)
 
         dict_insert = {"parkingLotName" : parking_lot_name,
